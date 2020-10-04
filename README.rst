c44ffddf298b3c878d72b5c18c9addf038dd2d24
Jonathan Roemer <dev@roemersoftworks.com>
2020-10-04 18:48:23 -0500

    Update README and setup
    We're all in DO now, and make setup take care of everything.

--- README.rst
+++ README.rst
@@ -1,3 +1,7 @@
 Dotfiles
 ========
-Baseline configuration files for my Raspberry Pi devices and Digital Ocean droplet.
+Baseline configuration files for my Digital Ocean droplets.
+
+I have no on-prem servers anymore, and any cloud servers are short-lived.
+
+Boxes are spun up via the Digital Ocean Web UI, and then setup.sh is grabbed via curl and ran.
--- setup.sh
+++ setup.sh
@@ -14,3 +14,6 @@ ssh-keygen -q -t rsa -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
 echo 'SHELL=/bin/sh \
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin \
 */5 * * * *   root curl https://github.com/pid1.keys > /home/root/.ssh/authorized_keys' > /etc/cron.d/keys
+
+curl https://raw.githubusercontent.com/pid1/dotfiles/master/tmux.conf > /root/.tmux.conf
+curl https://raw.githubusercontent.com/pid1/dotfiles/master/bash_aliases > /root/.bash_aliases
