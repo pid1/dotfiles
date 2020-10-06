41e19816e7fb0d3d91901e75f84b66e69c2e3097
Jonathan Roemer <dev@roemersoftworks.com>
2020-10-06 18:31:59 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,32 @@
 #! /usr/bin/env bash
 
+# Don't be noisy on login
 touch .hushlogin
+
+# Install what we need
+add-apt-repository ppa:neovim-ppa/stable -y
 apt update
 apt install build-essential \
             manpages-posix-dev \
             mosh \
+            neovim \
             tmux \
             unzip \
             valgrind
 
+# Set up host keys without user intervention
 ssh-keygen -q -t rsa -N '' <<< ""$'\n'"y" 2>&1 >/dev/null
 
+# SSH key management
 echo 'SHELL=/bin/sh \
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin \
 */5 * * * *   root curl https://github.com/pid1.keys > /home/root/.ssh/authorized_keys' > /etc/cron.d/keys
 
+# Configs
 curl https://raw.githubusercontent.com/pid1/dotfiles/master/tmux.conf > /root/.tmux.conf
 curl https://raw.githubusercontent.com/pid1/dotfiles/master/bash_aliases > /root/.bash_aliases
+
+curl -fLo /root/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p /root/.config/nvim
+curl https://raw.githubusercontent.com/pid1/dotfiles/master/init.vim > /root/.config/nvim/init.vim
